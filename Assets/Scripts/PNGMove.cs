802176c5c4d1f17dcfd4f4f3e3ee4c4b23faa28d
Neptoki <zoedn1911@gmail.com>
2025-03-18 19:57:10 +0000

    trigger collisi0on fixed, makes the whale move, but there is problem with sound starting on paly

--- Assets/Scripts/PNGMove.cs
+++ Assets/Scripts/PNGMove.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class ObjectMovement : MonoBehaviour
 {
-    public Vector3 destination; // Destination position (assign in the Unity editor)
-    public float speed = 2f; // Movement speed
+    public Vector3 destination;
+    public float speed = 2f;
 
-    void OnTriggerEnter(Collider other)
+    void Update()
     {
-        // Move the object towards the destination
         transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
     }
 }
