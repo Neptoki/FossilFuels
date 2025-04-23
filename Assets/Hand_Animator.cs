3913aeed5b8bedaca894740d13362e3dd914a6ab
Neptoki <zoedn1911@gmail.com>
2025-04-23 14:08:57 +0100

    third hand vid done, working colliders

--- Assets/Hand_Animator.cs
+++ Assets/Hand_Animator.cs
@@ -11,15 +11,20 @@ public class R_Hand_Animator : MonoBehaviour
 
     [SerializeField] private NearFarInteractor nearFarInteractor;
     [SerializeField] private SkinnedMeshRenderer handMesh;
+    [SerializeField] private GameObject handArmature;
     [SerializeField] private InputActionReference selectActionRef;
     [SerializeField] private InputActionReference activateActionRef;
     [SerializeField] private Animator handAnimator;
+    [SerializeField] private float actionDelay = 0.3f;
 
     private static readonly int activateAnim = Animator.StringToHash("activate");
     private static readonly int selectAnim = Animator.StringToHash("select");
 
+    private bool emptyHand;
+
     private void Awake()
     {
+        emptyHand = true;
         nearFarInteractor.selectEntered.AddListener(OnGrab);
         nearFarInteractor.selectExited.AddListener(OnRelease);
     }
@@ -27,18 +32,35 @@ public class R_Hand_Animator : MonoBehaviour
     private void OnGrab(SelectEnterEventArgs arg0)
     {
         Debug.Log("Selected");
-        handMesh.enabled = false;
+        emptyHand = false;
+        StartCoroutine(DelayedGrab());
     }
 
     private void OnRelease(SelectExitEventArgs arg0)
     {
+        StartCoroutine(DelayedRelease());
+    }
+    private IEnumerator DelayedGrab()
+    {
+        yield return new WaitForSeconds(actionDelay);
+        handMesh.enabled = false;
+        handArmature.SetActive(false);
+    }
+    private IEnumerator DelayedRelease()
+    {
+        yield return new WaitForSeconds(actionDelay);
         handMesh.enabled = true;
+        handArmature.SetActive(true);
+        emptyHand = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        handAnimator.SetFloat(activateAnim, activateActionRef.action.ReadValue<float>());
-        handAnimator.SetFloat(selectAnim, selectActionRef.action.ReadValue<float>());
+        if (emptyHand)
+        {
+            handAnimator.SetFloat(activateAnim, activateActionRef.action.ReadValue<float>());
+            handAnimator.SetFloat(selectAnim, selectActionRef.action.ReadValue<float>());
+        }
     }
 }
